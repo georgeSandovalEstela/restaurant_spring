e2b0e136372d080dab1f754bf0f6d5edca8572f5
George Sandoval <george.sandoval.estela@gmail.com>
2022-08-05 13:41:14 -0500

    add persons repository

--- src/main/java/com/dev/restaurants_v0/repository/PersonsRespository.java
+++ src/main/java/com/dev/restaurants_v0/repository/PersonsRespository.java
@@ -4,7 +4,5 @@ package com.dev.restaurants_v0.repository;
 import com.dev.restaurants_v0.domain.Persons;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PersonsRespository extends JpaRepository<Persons, Long> {
 }
