74e902a0f670fc04342afefcc226257762b2bffb
George Sandoval <george.sandoval.estela@gmail.com>
2022-08-08 19:17:23 -0500

    removing commetaries

--- src/main/java/com/dev/restaurants_v0/service/PersonalsService.java
+++ src/main/java/com/dev/restaurants_v0/service/PersonalsService.java
@@ -7,9 +7,4 @@ public interface PersonalsService {
     String savePersonals(PersonalsSaveRequest personalsSaveRequest)throws Exception;
     PersonalsResponse findPersonalsById(Long id)throws Exception;
     PersonalsResponse findPersonalsByDocumentNumber(String documentNumber)throws Exception;
-    /*List<Restaurants> getAllRestaurants()throws Exception;
-    String updateRestaurant(RestaurantUpdateRequest restaurantUpdateRequest) throws Exception;
-    Restaurants findRestaurantById(Long id)throws Exception;
-    String deleteRestaurantById(Long id)throws Exception;
-    RestaurantPersonalsResponse getPersonalsByRestaurantId(Long id) throws Exception;*/
 }
--- src/main/java/com/dev/restaurants_v0/service/PersonsService.java
+++ src/main/java/com/dev/restaurants_v0/service/PersonsService.java
@@ -7,9 +7,4 @@ import com.dev.restaurants_v0.dto.request.PersonsSaveRequest;
 public interface PersonsService {
     String savePersons(PersonsSaveRequest personsSaveRequest)throws Exception;
     Persons findPersonsByDocumentNumber(String documentNumber)throws Exception;
-    /*List<Restaurants> getAllRestaurants()throws Exception;
-    String updateRestaurant(RestaurantUpdateRequest restaurantUpdateRequest) throws Exception;
-    Restaurants findRestaurantById(Long id)throws Exception;
-    String deleteRestaurantById(Long id)throws Exception;
-    RestaurantPersonalsResponse getPersonalsByRestaurantId(Long id) throws Exception;*/
 }
