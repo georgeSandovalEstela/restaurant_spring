e456400a40a55bb733351ec1768a8271a0ebfcd6
George Sandoval <george.sandoval.estela@gmail.com>
2022-08-10 16:08:11 -0500

    test: find personals by documentnumber

--- src/test/java/com/dev/restaurants_v0/controller/PersonalsControllerTest.java
+++ src/test/java/com/dev/restaurants_v0/controller/PersonalsControllerTest.java
@@ -3,6 +3,8 @@ package com.dev.restaurants_v0.controller;
 import com.dev.restaurants_v0.dto.base.GlobalResponse;
 import com.dev.restaurants_v0.dto.request.PersonalsSaveRequest;
 import com.dev.restaurants_v0.dto.request.PersonsSaveRequest;
+import com.dev.restaurants_v0.dto.response.Personals.PersonalsResponse;
+import com.dev.restaurants_v0.dto.response.PersonsResponse.PersonsResponse;
 import com.dev.restaurants_v0.service.PersonalsService;
 import com.dev.restaurants_v0.utils.Codes;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Date;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -39,5 +42,20 @@ public class PersonalsControllerTest {
         when(personalsService.savePersonals(request)).thenReturn(Codes.PERSONS_ERROR_DOCUMENT);
         ResponseEntity<GlobalResponse> response = personalsController.savePersonals(request);
         assertEquals(Codes.PERSONS_ERROR_DOCUMENT, response.getBody().getError().getCode());
+    }
+
+    @Test
+    public void findPersonalsByDocumentNumber_SUCCESS()throws Exception{
+        PersonalsResponse personalsResponse = new PersonalsResponse(100L,"CAJERO",new Date(),new Date(),new Date(),new Date(),"Tunki",new PersonsResponse(100L,"George","Sandoval Estela","George Sandoval Estela","DNI","74901792",new Date(),"george.sandoval@email.com","MASCULINO","Los suaces","Peru","Tingo Maria"));
+        when(personalsService.findPersonalsByDocumentNumber("20987654672")).thenReturn(personalsResponse);
+        ResponseEntity<GlobalResponse> response = personalsController.findPersonalsByDocumentNumber("20987654672");
+        assertEquals(personalsResponse, response.getBody().getData());
+    }
+
+    @Test
+    public void findPersonalsByDocumentNumber_NOT_FOUND()throws Exception{
+        when(personalsService.findPersonalsByDocumentNumber("20987654672")).thenReturn(null);
+        ResponseEntity<GlobalResponse> response = personalsController.findPersonalsByDocumentNumber("20987654672");
+        assertTrue(Objects.isNull(response.getBody().getData()));
     }
 }
