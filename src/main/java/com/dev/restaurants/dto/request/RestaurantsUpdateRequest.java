a96e08ac19ff96f3e21c035de2031ecbbfdc7f50
George Sandoval <george.sandoval.estela@gmail.com>
2022-08-16 11:35:08 -0500

    test: findd restaurant by id

--- src/main/java/com/dev/restaurants/dto/request/RestaurantsUpdateRequest.java
+++ src/main/java/com/dev/restaurants/dto/request/RestaurantsUpdateRequest.java
@@ -2,9 +2,11 @@ package com.dev.restaurants.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 public class RestaurantsUpdateRequest {
--- src/test/java/com/dev/restaurants/service/RestaurantsServiceTest.java
+++ src/test/java/com/dev/restaurants/service/RestaurantsServiceTest.java
@@ -2,16 +2,24 @@ package com.dev.restaurants.service;
 
 import com.dev.restaurants.domain.Restaurants;
 import com.dev.restaurants.dto.request.RestaurantsSaveRequest;
+import com.dev.restaurants.dto.request.RestaurantsUpdateRequest;
 import com.dev.restaurants.repository.RestaurantsRepository;
 import com.dev.restaurants.utils.Codes;
+import com.dev.restaurants.utils.Constants;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.Optional;
 
 @SpringBootTest
 public class RestaurantsServiceTest {
@@ -38,5 +46,39 @@ public class RestaurantsServiceTest {
         String response = restaurantService.saveRestaurant(request);
 
         assertEquals(Codes.RESTAURANT_ERROR_NAME, response);
+    }
+    @Test
+    public void findRestaurantById_SUCCESS()throws Exception{
+        when(restaurantsRepository.findByIdAndState(100L,Integer.valueOf(Constants.STATE_ACTIVE))).thenReturn(Optional.of(new Restaurants()));
+        Restaurants response = restaurantService.findRestaurantById(100L);
+
+        assertFalse(Objects.isNull(response));
+    }
+
+    @Test
+    public void findRestaurantById_NOT_FOUND()throws Exception{
+        when(restaurantsRepository.findByIdAndState(100L,Integer.valueOf(Constants.STATE_ACTIVE))).thenReturn(Optional.empty());
+        Restaurants response = restaurantService.findRestaurantById(100L);
+
+        assertTrue(Objects.isNull(response));
+    }
+
+   /* @Test
+    public void updateRestaurant_SUCCESS_UPDATE()throws Exception{
+        Restaurants restaurants = new Restaurants(104L,"20987654324","Tunki","Restobar","Castillo",new Date(),new Date(),null,1);
+        RestaurantsUpdateRequest request = new RestaurantsUpdateRequest(104L,"20987654324","Tunki","RESTOBAR","DESCONOCIDO");
+        when(restaurantsRepository.findByIdAndState(104L,Integer.valueOf(Constants.STATE_ACTIVE))).thenReturn(Optional.of(restaurants));
+        when(restaurantsRepository.save(Mockito.any(Restaurants.class))).thenReturn(restaurants);
+        String response = restaurantService.updateRestaurant(request);
+
+        assertEquals(Codes.SUCCESS_UPDATE, response);
+    }*/
+
+    @Test
+    public void updateRestaurant_ERROR_ID()throws Exception{
+        when(restaurantsRepository.findByIdAndState(100L,Integer.valueOf(Constants.STATE_ACTIVE))).thenReturn(Optional.empty());
+        String response = restaurantService.updateRestaurant(new RestaurantsUpdateRequest());
+
+        assertEquals(Codes.COMMON_ERR_ID_001, response);
     }
 }
